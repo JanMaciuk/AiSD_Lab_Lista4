c9787d88ba25afbf0c68c3bf259e0d42c021657b
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2023-04-12 20:16:54 +0200

    Add stack copy with order preservation without extra stack

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -1,38 +1,41 @@
 public class ArrayStack<T> implements IStack<T> {
     int topIndex;
     T[] array;
+
     @SuppressWarnings("unchecked")
     public ArrayStack(int initialSize) {
-    array = (T[]) new Object[initialSize];
-    topIndex = 0;
+        array = (T[]) new Object[initialSize];
+        topIndex = 0;
     }
+
     @SuppressWarnings("unchecked")
     public ArrayStack() {
-    array = (T[]) new Object[999];
-    topIndex = 0;
+        array = (T[]) new Object[999];
+        topIndex = 0;
     }
+
     @Override
     public boolean isEmpty() {
-        return topIndex==0;
+        return topIndex == 0;
     }
 
     @Override
     public boolean isFull() {
-        return topIndex==array.length;
+        return topIndex == array.length;
     }
 
     @Override
     public T pop() throws EmptyStackException {
-        if(isEmpty())
+        if (isEmpty())
             throw new EmptyStackException();
         return array[--topIndex];
     }
 
     @Override
     public void push(T elem) throws FullStackException {
-        if(isFull())
+        if (isFull())
             throw new FullStackException();
-        array[topIndex++]=elem;
+        array[topIndex++] = elem;
 
     }
 
@@ -43,21 +46,36 @@ public class ArrayStack<T> implements IStack<T> {
 
     @Override
     public T top() throws EmptyStackException {
-        if(isEmpty())
+        if (isEmpty())
             throw new EmptyStackException();
-        return array[topIndex-1];
+        return array[topIndex - 1];
     }
-    public ArrayStack<T> reverse()  {
+
+    public ArrayStack<T> reverse() {
         ArrayStack<T> stack = new ArrayStack<>(size());
         int size = size();
         while (!isEmpty()) { // wycinam wszystkie elementy ze stosu i wklejam do nowego stosu
-            try { stack.push(pop()); }
-            catch (Exception e) { e.printStackTrace(); } //jest warunek !isEmpty i ten sam rozmiar, więc nie powinno dojść do błędu
+            try {
+                stack.push(pop());
+            } catch (Exception e) {
+                e.printStackTrace();
+            } //jest warunek !isEmpty i ten sam rozmiar, więc nie powinno dojść do błędu
         }
         topIndex = size; // przywracam oryginalny stos do pierwotnej formy
         //array = stack.array;  //to by było, gdybym nadpisywał oryginalny stos.
         return stack;
     }
 
+    public ArrayStack<T> reverseOverwrite() {
+        ArrayStack<T> stack = new ArrayStack<>(size());
+        while (!isEmpty()) { // wycinam wszystkie elementy ze stosu i wklejam do nowego stosu
+            try {
+                stack.push(pop());
+            } catch (Exception e) {
+                e.printStackTrace();
+            } //jest warunek !isEmpty i ten sam rozmiar, więc nie powinno dojść do błędu
+        }
+        //array = stack.array;  //to by było, gdybym nadpisywał oryginalny stos.
+        return stack;
+    }
 }
-
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main { //TODO zadanie za 4 punkty, kopiowanie używając tylko zmiennych
@@ -12,7 +13,8 @@ public class Main { //TODO zadanie za 4 punkty, kopiowanie używając tylko zmie
                             1. Sprawdź zrównoważenie nawiasów w wyrażeniu
                             2. Sprawdzenie czy wyrażenie jest polindromem
                             3. Przenoszenie elementów stosu przy użyciu stosu pomocniczego
-                            4. Przenoszenie elementów stosu przy użyciu zmiennych""");
+                            4. Przenoszenie elementów stosu przy użyciu zmiennych
+                            5. "Sprytne" przenoszenie elementów stosu""");
             int wybor = scan.nextInt();
             scan.nextLine();
             switch (wybor) {
@@ -20,13 +22,19 @@ public class Main { //TODO zadanie za 4 punkty, kopiowanie używając tylko zmie
                 case 1 -> zrownowazenieCheck();
                 case 2 -> polindromCheck();
                 case 3 -> przenoszeniePomocniczyStos();
-                case 4 -> {}//todo
+                case 4 -> przenoszenieZmienne();
+                case 5 -> przenoszenieSprytne();
                 default -> System.out.println("Zły numer opcji");
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             e.printStackTrace();
-// To nie jest dobry sposób obsługi błędów, ale w tych metodach wszystkie stosy mają sprawdzaną długość, i są w warunkach sprawdzających że nie są puste
-        }}
+            if (e instanceof InputMismatchException) {
+                System.out.println("No i zobacz co narobiłeś, wpisanie cyfry takie trudne?");
+                return;
+            }
+        }// To nie jest dobry sposób obsługi błędów, ale w tych metodach wszystkie stosy mają sprawdzaną długość, i są w warunkach sprawdzających że nie są puste
+        }
     }
 
     public static void zrownowazenieCheck() throws FullStackException, EmptyStackException {
@@ -71,12 +79,59 @@ public class Main { //TODO zadanie za 4 punkty, kopiowanie używając tylko zmie
 
     }
 
-    public static void przenoszenieZmienne() { //TODO
+    public static void przenoszenieZmienne()  throws FullStackException, EmptyStackException {
+        System.out.println("podaj jakiś ciąg znaków do testów:");
+        String napis = scan.nextLine();
+        ArrayStack<Character> S1 = new ArrayStack<>(napis.length());
+        ArrayStack<Character> S2 = new ArrayStack<>(napis.length());
+        Character zmiennaPomocnicza;
+
+        for (char ch: napis.toCharArray()) { S1.push(ch); } // Zapisuje wszystkie znaki z napisu do stosu oryginalnego
+
+        if(!S1.isEmpty()){
+            int i =  S1.size()-1;
+
+            for(int x =0; x< i ;x++){
+                zmiennaPomocnicza = S1.pop();
+                S2 = S1.reverseOverwrite();
+                for(int y = 0; y < x; y++){
+                    S1.push(S2.pop());
+                }
+                S1.push(zmiennaPomocnicza);
+                while(!S2.isEmpty()){
+                    S1.push(S2.pop());
+                }
+
+            }
+            S2 = S1.reverseOverwrite();
+        }
+
+        for (char ch: napis.toCharArray()) { S1.push(ch); }  //Przywracam oryginalny stos do porównania
+        while (!S1.isEmpty()) {
+            //Sprawdzam czy elementy są takie same, jeżeli tak to są w dobrej kolejności
+            if (S1.pop() != S2.pop()) { System.out.println("Błąd");return; }
+        }
+        System.out.println("Przenoszenie udane");
+    }
+    public static void przenoszenieSprytne()  throws FullStackException, EmptyStackException {// Najprostsza wersja, nawet bez zmiennej.
         System.out.println("podaj jakiś ciąg znaków do testów:");
         String napis = scan.nextLine();
         ArrayStack<Character> S1 = new ArrayStack<>(napis.length());
         ArrayStack<Character> S2 = new ArrayStack<>(napis.length());
 
 
+        for (char ch: napis.toCharArray()) { S1.push(ch); } // Zapisuje wszystkie znaki z napisu do stosu oryginalnego
+
+        if(!S1.isEmpty()){
+            S1 = S1.reverseOverwrite();
+            S2 = S1.reverseOverwrite();
+        }
+
+        for (char ch: napis.toCharArray()) { S1.push(ch); }  //Przywracam oryginalny stos do porównania
+        while (!S1.isEmpty()) {
+            //Sprawdzam czy elementy są takie same, jeżeli tak to są w dobrej kolejności
+            if (S1.pop() != S2.pop()) { System.out.println("Błąd");return; }
+        }
+        System.out.println("Przenoszenie udane");
     }
 }
